f05408ee309e39d872df49a52edfabb2f6b00ed2
Marcus Chiu <marcuschiu9@gmail.com>
2021-03-10 17:41:36 -0600

    initial commit

---
+++ src/main/java/com/marcuschiu/jenkinsexampleprojecta/JenkinsExampleProjectAApplication.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.jenkinsexampleprojecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleProjectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleProjectAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcuschiu/jenkinsexampleprojecta/JenkinsExampleProjectAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.jenkinsexampleprojecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleProjectAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
