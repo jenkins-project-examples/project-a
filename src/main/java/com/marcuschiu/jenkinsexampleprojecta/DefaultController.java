6bb2d3f582e8fc1085610bc25810dc745d3921e9
Marcus Chiu <marcuschiu9@gmail.com>
2021-03-10 18:01:23 -0600

    added DefaultController

---
+++ src/main/java/com/marcuschiu/jenkinsexampleprojecta/DefaultController.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.jenkinsexampleprojecta;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/")
+    public String home() {
+        return "success";
+    }
+}
